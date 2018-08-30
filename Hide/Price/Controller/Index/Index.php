fdd32a025de9bcf02eb6ccb86c1d99e1e9a53a85
wclansar <ansar.husain@williamscommerce.com>
2018-08-30 11:56:28 +0530

    first commit

---
+++ Hide/Price/Controller/Index/Index.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Hide\Price\Controller\Index;
+
+class Index extends \Magento\Framework\App\Action\Action
+{
+   protected $_pageFactory;
+
+   public function __construct(
+    \Magento\Framework\App\Action\Context $context,
+    \Magento\Framework\View\Result\PageFactory $pageFactory
+    )
+   {
+       $this->_pageFactory = $pageFactory;
+       return parent::__construct($context);
+   }
+
+  public function execute()
+   {
+      
+       $page =  $this->_pageFactory->create();
+       return $page;
+   }
+}
